4bc49931f907b0ff91bbafeed3c0ec8deb016373
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-07-13 00:00:31 +0100

    Changed the global scrollbar styling
    -Adding some custom styling for the scrollbar.
    -Did some more minor work on the works component carousel, will finish tomorrow.

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -30,12 +30,13 @@ const Works = () => {
             <div className='w-full border-b-2 border-stone-500' />
             <p className='mt-3'>I always want to keep myself busy, meaning a project is always in the proverbial oven. Have a look at some of the websites and designs I’ve worked on recently:</p>
 
-            <div className='carousel mt-10 w-full h-full'>
-                <div className='inner-carousel flex flex-row justify-center items-center'>
+            <div className='carousel mt-10 w-full h-full overflow-x-auto'>
+                <div className='inner-carousel flex flex-col md:flex-row justify-start items-center gap-y-6 md:gap-y-0 md:gap-x-6 w-[2500px]'>
                     {works.map((work, index) => (
-                        <div className='project bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg z-0' key={index}>
-                            <h1 className='text-[3rem]'>{work.title}</h1>
-
+                        <div className={`flex flex-col justify-start items-start project w-full px-4 pb-4 rounded-md bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg z-0`} key={index}>
+                            <h1 className='text-[3rem] text-start'>{work.title}</h1>
+                            <img className='my-4 w-full max-h-[15rem]' src={work.image} alt={work.title} />
+                            <p className=''>{work.description}</p>
                         </div>
                     ))}
                 </div>
