248dcf39c56f0f7bdc2c3aa737715ed8ffd68940
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-08-23 21:10:27 +0100

    Decoupled the Project and ProjectContent components
    -Moved the useParams into the child component to decouple the project and projectContent components.

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -4,12 +4,9 @@ import { HoverContext } from "../utils/HoverContext";
 import ProjectNav from "../components/projectNav/ProjectNav";
 import { gsap } from "gsap";
 import ProjectContent from "../components/projectContent/ProjectContent";
-import { useParams } from "react-router-dom";
 
 const Project = () => {
   const { handleClick } = useContext(HoverContext);
-
-  const { id } = useParams();
 
   useEffect(() => {
     handleClick();
@@ -31,7 +28,7 @@ const Project = () => {
     <div className="project place-self-center">
       <Mouse />
       <ProjectNav />
-      <ProjectContent data={id} />
+      <ProjectContent />
     </div>
   );
 };
